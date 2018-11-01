ab95a9fb9b321652d2490040103735589ccb5fe8
Mike Frank <mfrank@salesforce.com>
2018-11-01 15:12:52 -0400

    Twig?

--- web/index.php
+++ web/index.php
@@ -22,4 +22,9 @@ $app->get('/', function() use($app) {
   return $app['twig']->render('index.twig');
 });
 
+$app->get('/opt-in-poc', function() use($app) {
+    $app['monolog']->addDebug('logging output.');
+    return $app['twig']->render('index.twig');
+});
+
 $app->run();
